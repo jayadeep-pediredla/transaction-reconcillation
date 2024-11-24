efd8d5192d815e9838b5485d2fb0cb4a58bcc5b7
jayadeep-pediredla <hii.jayadeep@gmail.com>
2024-11-24 23:03:05 +0530

    Update reconciliationService.js

--- src/services/reconciliationService.js
+++ src/services/reconciliationService.js
@@ -1,1 +1,17 @@
+function reconcileTransactions( bankTransactions,internalTransactions,timeToleranceMinutes=5){
+  const matched=[]
+  const unmatched=[...bankTransactions]
+  
+  const toleranceMs=timeToleranceMinutes*60*1000
+  
+internalTransaction.forEach((internalTx)=>{
+const match=bankTransactions.find((banktx)=>{
+  const amountMatches =banktx.amount=== internalTx.amount
+  const 
+})
+  
+})
+  
+  
+}
 
