3576f723ec3fd89d45b9aca843055915177454ec
jayadeep-pediredla <hii.jayadeep@gmail.com>
2024-11-24 22:49:28 +0530

    Update bankData.js

--- src/data/bankData.js
+++ src/data/bankData.js
@@ -1,1 +1,9 @@
-
+module.exports=[
+  {
+    id:1,amount:100, timestamp:
+  },{id:2, amount:200,timestamp:}
+  ,{id:3,amount:150,timestamp:}
+  ]
+// here id is unique identifier for transactions 
+// here amount is the amount is giong to be  transacted 
+// timestamp is the the thing here in which time transaction is occured 
